4c84c08440600117e873dbb547d15ea0946f6074
net-pixel <masataka.for.ofua@gmail.com>
2020-07-20 01:56:15 +0900

    Routes & Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_162417) do
+ActiveRecord::Schema.define(version: 2020_07_19_165250) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+    t.index ["name"], name: "index_categories_on_name"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
