cfdb6d70f3ae840af706cf789cc0f400b3927bf8
net-pixel <masataka.for.ofua@gmail.com>
2020-08-03 00:10:24 +0900

    PayjpGem

--- db/migrate/20200720143250_create_cards.rb
+++ db/migrate/20200720143250_create_cards.rb
@@ -1,6 +1,7 @@
 class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
+      t.bigint :user_id, foreign_key: true
       t.string :customer_id, null: false
       t.string :card_id, null: false
 
