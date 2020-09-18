0ec6f3a974598afbb2232a4648225c48cc251d31
net-pixel <masataka.for.ofua@gmail.com>
2020-09-18 13:36:54 +0900

    FK

--- db/schema.rb
+++ db/schema.rb
@@ -27,11 +27,12 @@ ActiveRecord::Schema.define(version: 2020_09_15_053240) do
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "cart_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -100,6 +101,7 @@ ActiveRecord::Schema.define(version: 2020_09_15_053240) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
   add_foreign_key "cart_details", "carts"
   add_foreign_key "cart_details", "products"
   add_foreign_key "images", "products"
