5fe9a74c2c3e1d0a28adc13c8dff8b65d6f661f0
net-pixel <masataka.for.ofua@gmail.com>
2020-09-23 01:02:29 +0900

    GuideBasic

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,64 @@
+class PagesController < ApplicationController
+  before_action :set_cart
+
+  def about
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      @address = Address.find(current_user.id)
+    end
+  end
+
+  def privacy
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      @address = Address.find(current_user.id)
+    end
+  end
+
+  def faq
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      @address = Address.find(current_user.id)
+    end
+  end
+
+  def delivery
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      @address = Address.find(current_user.id)
+    end
+  end
+
+  def refund
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      @address = Address.find(current_user.id)
+    end
+  end
+
+  def terms_of_service
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      @address = Address.find(current_user.id)
+    end
+  end
+
+  def tokushoho
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      @address = Address.find(current_user.id)
+    end
+  end
+
+  def inquiry
+    if user_signed_in?
+      @user = User.find(current_user.id)
+      @address = Address.find(current_user.id)
+    end
+  end
+
+  private
+  def set_cart
+    @cart = current_cart
+  end
+end
