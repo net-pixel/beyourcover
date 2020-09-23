0c92b2176eae6ad433b9f8bc34712866e3210822
net-pixel <masataka.for.ofua@gmail.com>
2020-09-24 00:05:07 +0900

    lock

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,13 +4,15 @@ class OrderDetail < ApplicationRecord
 
 
   def self.adjustStock_createDetail(order, cart_details)
-    cart_details.target.each do |product|
+    cart_details.target.each do |cart_detail|
       OrderDetail.create!(
-        order_id: order.id, product_id: product[:product_id], quantity: product[:quantity]
+        order_id: order.id, product_id: cart_detail[:product_id], quantity: cart_detail[:quantity]
       )
-      @product = Product.find_by(id: product[:product_id])
-      @product.update(stock: (@product.stock - product[:quantity]))
-      CartDetail.find(product[:id]).delete
+      @product = Product.find_by(id: cart_detail[:product_id])
+      @product.with_lock do
+        @product.update(stock: (@product.stock - cart_detail[:quantity]))
+      end
+      CartDetail.find(cart_detail[:id]).delete
     end
   end
 
