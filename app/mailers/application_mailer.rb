b3052a3ba5537de8691e7d3a8481fc90b88617ba
net-pixel <masataka.for.ofua@gmail.com>
2020-09-27 21:40:19 +0900

    test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'craftclwd@gmail.com'
   layout 'mailer'
+
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  rescue StandardError => e
+    # ↓が実行されると思っていたが実行されなかった、何故？？？
+    Rails.logger.error("failed send mail. #{e.message}")
+  end
 end
