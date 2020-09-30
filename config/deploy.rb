5b60503f7098af86ebe757d43c82cf7b9a3bc624
net-pixel <masataka.for.ofua@gmail.com>
2020-09-30 15:09:17 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :keep_releases, 5
 set :pty, true
 
 #credentials.yml.encの読み込み
-set :linked_files, %w{ config/credentials.yml.enc }
+# set :linked_files, %w{ config/credentials.yml.enc }
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
